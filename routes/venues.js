26a13c6d1f75e306cca7e7a73855635eb3f2d49e
Eesh Chilukalapalli <eeshlikhith@gmail.com>
2017-09-12 21:21:42 +0530

    fix typo in route

--- routes/venues.js
+++ routes/venues.js
@@ -4,7 +4,7 @@ var init = function (app) {
   app.post('/venues', venueController.addVenue)
   app.get('/venues', venueController.getAllVenues)
   app.post('/venues/update', venueController.editVenue)
-  app.post('/venues/delte', venueController.deleteVenue)
+  app.post('/venues/delete', venueController.deleteVenue)
 }
 
 module.exports = init
