d0ad42287adabe4fb5ece7315f84af219b278801
Eesh Chilukalapalli <eeshlikhith@gmail.com>
2017-12-09 16:55:10 +0530

    Add confirmation and rejection endpoints

--- routes/reservation.js
+++ routes/reservation.js
@@ -6,7 +6,7 @@ var init = function (app) {
   app.post('/reservation/cancel', reservationController.cancelReservation)
 
   app.get('/reservation/all', reservationController.getAllReservations)
-  app.post('/reservation/confirmation', reservationController.confirmReservation)
+  app.post('/reservation/confirm', reservationController.confirmReservation)
   app.post('/reservation/reject', reservationController.rejectReservation)
 }
 
