836b30a2ce306a15868692a7812fbc558ccaf380
Eesh Chilukalapalli <eeshlikhith@gmail.com>
2018-01-04 03:51:13 +0530

    include user route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 var venues = require('./venues')
 var auth = require('./auth')
 var reservation = require('./reservation')
+var user = require('./user')
 
 var attachRoutes = function attachRoutes(app) {
   auth(app)
   venues(app)
   reservation(app)
+  user(app)
 }
 
 module.exports = attachRoutes
